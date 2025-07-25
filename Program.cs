4908e96bebb2428ca8b7c2c3025b27786a37745d
Jonathan Bodrero <jbodrero@hotmail.com>
2025-07-25 11:35:01 -0600

    Initial setup

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//  Jonathan Bodrero
+//  July 25, 25
+//  Lab 10  ATM
+
+string[] readBankFile = File.ReadAllLines("bank.txt");  // Read in bank database
+List<(string, int, double)> customerData = new List<(string userName, int PIN, double balance)>();
+string userName;
+int PIN;
+double balance;
+
+
+
+
+
+
