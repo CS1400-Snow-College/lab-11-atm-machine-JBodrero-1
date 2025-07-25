219c4596c3e86bb25d9603276dfc01c97e38360f
Jonathan Bodrero <jbodrero@hotmail.com>
2025-07-25 12:43:25 -0600

    Basic login success.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 //  July 25, 25
 //  Lab 10  ATM
 
+
 string[] readBankFile = File.ReadAllLines("bank.txt");  // Read in bank database
 List<(string userName, int PIN, double balance)> customerData = new List<(string userName, int PIN, double balance)>();
 // string userName;
@@ -28,18 +29,50 @@ foreach (string customer in readBankFile)
 // customerData.ForEach(thing => Console.Write($"{thing}, ")); // Cycles through list, printing each item.
 // foreach (string customer in readBankFile)
 
+
+
 foreach (var customer in customerData)
 {
     Console.WriteLine($"{customer.userName} has a PIN of {customer.PIN} with a balance of ${customer.balance}.");
 }
 
-
-
-/*
-for (int i = 0; i < customerData.Count; i++)
+Console.Clear();
+Console.WriteLine("Welcome to BadgerBank.");
+if (ValidLogin(customerData) !=-1)
 {
-    Console.WriteLine(customerData[i]);
+    Console.WriteLine("Thank you for logging in. ");
 }
-*/
 
+
+
+int ValidLogin(List<(string userName, int PIN, double balance)> customerData)
+{
+
+    string inputUser;
+    int inputPIN;
+    int location; 
+    Console.Write("Please enter your userName: ");
+    inputUser = Console.ReadLine();
+    Console.Write("Please enter your PIN: ");
+    while (!Int32.TryParse(Console.ReadLine(), out inputPIN))  //  Check if input is an integer
+    {
+        Console.Write("Oops. That is not a number. Try starting over.");
+        return -1;
+    }
+    List<string> userTemp = new List<string>();
+    foreach (var user in customerData)
+    {   userTemp.Add(user.userName); }
+
+    if (userTemp.Contains(inputUser))
+    {
+        location = userTemp.IndexOf(inputUser);
+        return location;
+    }
+    else
+    {
+        Console.WriteLine("That is not a valid username / PIN number combination. Try starting over.");
+        return -1;
+    }
+
+}
 
--- obj/Debug/net9.0/lab-11-atm-machine-JBodrero-1.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab-11-atm-machine-JBodrero-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-11-atm-machine-JBodrero-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4908e96bebb2428ca8b7c2c3025b27786a37745d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b500185b4daaf6cca15d3c6cc95c6b9e5d62ef63")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-11-atm-machine-JBodrero-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-11-atm-machine-JBodrero-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
